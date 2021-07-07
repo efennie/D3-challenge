9e7d27b39258a97bbf90840c886176272fe0ee3a
efennie <efennie@email.arizona.edu>
2021-07-06 21:38:26 -0700

    trying to add labels to the graph

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -22,7 +22,7 @@ var chartGroup = svg.append("g")
 
 // Import Data
 
-d3.csv("/Users/bitty/D3-chalenge/D3-challenge/D3_data_journalism/assets/data/data.csv").then(function(stateData) {
+d3.csv("/data/data.csv").then(function(stateData) {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -34,11 +34,11 @@ d3.csv("/Users/bitty/D3-chalenge/D3-challenge/D3_data_journalism/assets/data/dat
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(stateData, d => d.poverty)])
+      .domain([8, d3.max(stateData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(stateData, d => d.healthcare)])
+      .domain([4, d3.max(stateData, d => d.healthcare)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -65,7 +65,16 @@ d3.csv("/Users/bitty/D3-chalenge/D3-challenge/D3_data_journalism/assets/data/dat
     .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "10")
     .attr("fill", "blue")
-    .attr("opacity", ".5");
+    .attr("opacity", ".5")
+    .append("text")
+    .attr("class", "label")
+    .text(function(d) {return d.abbr; });
+
+    //adding state abbreviations to our circles
+    //.attr("text", d=> (d.abbr))
+
+
+
 
     // Step 6: Initialize tool tip
     // ==============================
@@ -94,15 +103,15 @@ d3.csv("/Users/bitty/D3-chalenge/D3-challenge/D3_data_journalism/assets/data/dat
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 0 - (height / 1.3))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+      .text("% of People without Healthcare");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${width / 2.5}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
+      .text("% of Population in Poverty");
   }).catch(function(error) {
     console.log(error);
   });
